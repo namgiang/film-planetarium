1dad429708baf15673accf9fd9f4a5dd8602a689
namgiang <namgiang0606@gmail.com>
2017-07-08 23:16:51 +0200

    add Spinning Loader when loading posters

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ReactTooltip from 'react-tooltip';
+import Loader from 'react-loader';
 import './MovieList.css';
 
 import MoviesService from '../../services/movies.js';
+
+const spinnerOptions = {
+    lines: 8,
+    length: 8,
+    width: 4,
+    radius: 10,
+    scale: 1.00,
+    corners: 1,
+    color: '#fff',
+    opacity: 0.25,
+    rotate: 0,
+    direction: 1,
+    speed: 1,
+    trail: 60,
+    fps: 20,
+    zIndex: 10,
+    left: '50%',
+    shadow: false,
+    hwaccel: false,
+    position: 'relative'
+};
 
 class MovieList extends Component {
 	constructor(props) {
@@ -33,7 +55,9 @@ class MovieList extends Component {
 		return (this.props.imageUrls !== []) ? (
 			<div className="movies-container" key={this.props.director.name}>
 			  { this.renderMovieSummary() }
-				{ this.renderPosters() }
+			  <Loader loaded={this.props.loaded} options={spinnerOptions} className="spinner">
+				  { this.renderPosters() }
+				</Loader>
 				{ this.state.movieHovered && this.renderMovieDescription() }
 			</div>			
 		) : (<div></div>);
@@ -106,6 +130,7 @@ class MovieList extends Component {
 function mapStateToProps(state) {
 	console.log(state);
   return {
+  	loaded: !state.filmApp.posters.isFetching,
     posters: state.filmApp.posters.urls
   };
 }
